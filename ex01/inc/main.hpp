fb57e9be3611487271368c206a37252b9253e29e
Iker Sojo <ikersojo@hotmail.com>
2023-05-30 23:55:52 +0200

    update

---
+++ ex01/inc/main.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isojo-go <isojo-go@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/21 17:37:02 by isojo-go          #+#    #+#             */
+/*   Updated: 2023/05/30 22:31:44 by isojo-go         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_HPP
+	#define MAIN_HPP
+	#ifndef DEBUG
+		#define DEBUG 0
+	#endif
+	#include "../classes/inc/Claptrap.hpp"
+	#include <iostream>
+#endif // MAIN_HPP
