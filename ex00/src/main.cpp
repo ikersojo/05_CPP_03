26a8047e7b4ef8b66c44c971dc2b96c0057c5673
Iker Sojo <ikersojo@hotmail.com>
2023-05-30 23:37:54 +0200

    update

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,23 +6,24 @@
 /*   By: isojo-go <isojo-go@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 00:35:47 by isojo-go          #+#    #+#             */
-/*   Updated: 2023/05/30 23:29:43 by isojo-go         ###   ########.fr       */
+/*   Updated: 2023/05/30 23:37:52 by isojo-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/main.hpp"
 
-static void	printYellowLine(std::string str)
+static void	printBlueLine(std::string str)
 {
-	std::cout  << std::endl << "\033[0;33m" << str
+	std::cout  << std::endl << "\033[0;94m" << str
 				<< "\033[0;39m" << std::endl;
 }
 
 int main(void)
 {
-	printYellowLine("[ -------- CHECKING CLAPTRAP CLASS -------- ]");
+	printBlueLine("[ -------- CHECKING CLAPTRAP CLASS -------- ]");
 	Claptrap cp1("iker");
 
+	std::cout << cp1 << std::endl;
 	
 	return (0);
 }
